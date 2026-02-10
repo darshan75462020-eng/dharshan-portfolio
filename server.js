c063b72c9ee0a6d4608b9e565341883192a38c6d
Dharshan <darshan75462020@gmail.com>
2026-02-10 23:26:16 +0530

    Update server.js
    cors

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ dotenv.config();
 const app = express();
 
 // middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // later you can restrict to frontend URL
+    methods: ["GET", "POST"],
+  })
+);
+
 app.use(express.json());
 
 // health check route
@@ -69,3 +75,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Backend running on port ${PORT}`);
 });
+
